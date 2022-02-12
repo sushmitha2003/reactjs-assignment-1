9e1bae44db0aad36a14849f76a9a53501e4c7729
sushmitha <20b01a1248@svecw.edu.in>
2022-02-12 10:51:49 +0530

    assignment1 from local to remote repo

---
+++ src/Script.js
@@ -0,0 +1,13 @@
+function Script() {
+    return (
+      <div className="liz">
+        <h1>Lizard</h1>
+        <h4>
+           Lizards are a widespread group of squamate <br></br> reptiles,with over
+          6,000 species,ranging across<br></br>all continents except Antarctica
+        </h4>
+        <h3>SHARE &nbsp; &nbsp; LEARNMORE</h3>
+      </div>
+    )
+  }
+  export default Script;
